7c631a068e4f8825a023cbd1b2d2a0467efb5fe1
lixingwang <lixingwang@yiche.com>
2023-03-06 15:43:25 +0800

    update logger implement

--- go.mod
+++ go.mod
@@ -1,10 +1,18 @@
 module github.com/okayes/gopkgs
 
-go 1.15
+go 1.20
 
 require (
 	github.com/go-redis/redis/v8 v8.11.4
 	github.com/streadway/amqp v1.0.0
 	gorm.io/driver/mysql v1.2.2
 	gorm.io/gorm v1.22.4
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.3 // indirect
 )
--- logger/logger.go
+++ logger/logger.go
@@ -10,36 +10,17 @@ import (
 var handle HandleFunc
 
 type Option struct {
-	FilePath, FileName string
-	Handle             HandleFunc
+	writer io.Writer
+	Handle HandleFunc
 }
 
 type HandleFunc func(string)
 
 func Init(option Option) {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-
 	handle = option.Handle
 
-	if _, err := os.Stat(option.FilePath); err != nil {
-		if os.IsNotExist(err) {
-			if err := os.MkdirAll(option.FilePath, os.ModePerm); err != nil {
-				log.Println(err)
-				return
-			}
-		} else {
-			log.Println(err)
-			return
-		}
-	}
-
-	file, err := os.Create(option.FilePath + option.FileName)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	log.SetOutput(io.MultiWriter(file, os.Stdout))
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetOutput(io.MultiWriter(option.writer, os.Stdout))
 }
 
 func Error(em string) {
